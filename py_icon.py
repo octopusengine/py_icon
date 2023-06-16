6f36ad4df190f62010a63907b4d118b65b7236ea
Honza S. Copak <bonum.comunae@volny.cz>
2023-06-16 11:50:25 +0200

    typo, clean up, fix bmp load format

--- py_icon.py
+++ py_icon.py
@@ -2,16 +2,17 @@
 """
 https://github.com/octopusengine/py_icon
 """
+import os
 import pygame
 import numpy as np
 import random
 
-__version__ = "0.1.0" # 2023/06
+__version__ = "0.1.2" # 2023/06
 
 icon_w, icon_h = 32, 32 #  32,32 / 16,16 / 16,32
 path = "data_img/"
-image_path = f"{path}test{icon_w}x{icon_h}.bmp"
-matrix_path = f"{path}matrix{icon_w}x{icon_h}.txt"
+image_path = f"{path}test_{icon_w}.bmp"
+matrix_path = f"{path}matrix{icon_w}.txt"
 
 # Size of pixels in the editor
 PIXEL_SIZE = 10
@@ -33,16 +34,34 @@ ic = (icon_w * PIXEL_SIZE + x0) // 2
 fstep = 23
 
 input_text = ""
+mode = 1
 my_icon_matrix_load = []
 
 # test matrix
 my_icon_matrix = [
-    "1000100110001001",
-    "0100010011000100",
-    "0010001100110001",
-    "0001001001100010",
+    "10000000000000010000000000000001",
+    "01000000000000010000000000000010",
+    "00100000000000010000000000000100",
+    "00010000000000010000000000001000",
+    "00001000000000010000000000010000",
+    "00000100000000010000000000100000",
+    "00000100000000000000000000100000",
+    "00001000000000000000000000010000",
+    "00010000000000000000000000001000",
+    "00100000000000000000000000000100",
+    "01000000000000000000000000000010",
+    "01000000000000010000000000000010",
+    "00100000000000010000000000000100",
+    "00010000000000010000000000001000",
+    "00001000000000010000000000010000",
+    "00000100000000010000000000100000",
+    "00000100000000000000000000100000",
+    "00001000000000000000000000010000",
+    "00010000000000000000000000001000",
+    "00100000000000000000000000000100",
+    "01000000000000000000000000000010",
+    "10000000000000000000000000000001",
 ]
-
 
 pygame.init()
 
@@ -50,7 +69,7 @@ pygame.init()
 #window_width, window_height = 16 * PIXEL_SIZE, 16 * PIXEL_SIZE
 resize = 3
 window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption(f"py_icon (v.{__version__})")
+pygame.display.set_caption(f"py_icon (ver. {__version__})")
 window.fill((0, 0, 0))
 
 # Initialize an empty icon
@@ -88,16 +107,16 @@ def draw_input_field():
 
 
 def draw_edit_icon():
+    pygame.draw.rect(window, BLACK2, (0, 0, window_width, 390))
     draw_text(f"icon {icon_w}x{icon_h}",x0, y0 -30, SILVER2)
     # window.fill((255, 255, 255))  # Clear the window content
     
     for y in range(icon_h):
         for x in range(icon_w):
-            if icon_data[x][15-y]:
+            if icon_data[x][y]: # 15-y ???
                 pygame.draw.rect(window, (0, 0, 0), (x0 + x * PIXEL_SIZE, y0 + y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
             else:
                 pygame.draw.rect(window, (255, 255, 255), (x0 + x * PIXEL_SIZE, y0 + y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
-
 
     pygame.draw.rect(window, (0, 128, 255), (x0, y0, icon_w * PIXEL_SIZE, icon_h * PIXEL_SIZE), 2)
 
@@ -106,12 +125,12 @@ def draw_edit_icon():
     pygame.draw.rect(window, BLACK2, (xc-5, y0, rect_width, rect_height))
     draw_text("CTRL + command:",xc, y0, COLOR2)
     draw_text("C/F - Clear / Fill",xc, y0+fstep*1)
-    draw_text(".   - ...", xc, y0+fstep*2)
+    draw_text("R   - Resize 16/32", xc, y0+fstep*2)
     draw_text("I   - Invert",xc, y0+fstep*3)
-    draw_text("N   - Noise",xc, y0+fstep*4)
+    draw_text("N/B - Noise / Border",xc, y0+fstep*4)
     draw_text("Z/X - Resize prew.",xc, y0+fstep*5)
-    draw_text("D   -  Data", xc, y0+fstep*6)
-    draw_text("M   -  Matrix",xc, y0+fstep*7)
+    draw_text("D   -  Data (export)", xc, y0+fstep*6)
+    draw_text("M   -  Matrix (add)",xc, y0+fstep*7)
     draw_text("L/S -  Load / Save", xc, y0+fstep*8)
     draw_text("Q   -  Quit", xc, y0+fstep*9)
 
@@ -135,25 +154,45 @@ def draw_edit_icon():
     pygame.display.flip()
 
 
+def icon_mode(mode,icon_w,icon_h):
+    mode += 1
+    if mode > 2: 
+        mode = 1
+    
+    if mode == 2: icon_w, icon_h = 16,16
+    if mode == 1: icon_w, icon_h = 32,32
+    draw_status(f"Mode > {mode}: {icon_w}x{icon_h}")
+
+    return mode, icon_w, icon_h
+
+
 def load_icon():
     draw_status("Load >")
-    draw_status2(f"{image_path}")
+    if os.path.exists(image_path):
+        size = os.path.getsize(image_path)
+        draw_status2(f"{image_path} ({size} Bytes)")
+    else:    
+        draw_status2(f"The file does not exist.")
     try:
-        icon_surface = pygame.image.load(image_path)
+        # icon_surface = pygame.image.load(image_path) #ok
+        icon_surface = pygame.image.load(image_path).convert_alpha()
         #  icon_surface = pygame.transform.flip(icon_surface, False, True)  # Horizontal flip (for correct orientation)
 
         # Convert the icon to a data array
-        icon_array = pygame.surfarray.array2d(icon_surface)
+        #icon_array = pygame.surfarray.array2d(icon_surface)
+        icon_array = pygame.surfarray.pixels2d(icon_surface)
         icon_array = np.asarray(icon_array, dtype=np.uint8)
 
         # Set the values in the editor icon based on the data array
         for y in range(icon_h):
             for x in range(icon_w):
-                icon_data[x][15-y] = True if icon_array[x][y] < 128 else False
-
+                #icon_data[x][icon_h-y] = True if icon_array[x][y] < 128 else False
+                icon_data[x][y] = True if icon_array[x][y] < 128 else False
+                             ###15
         print("The icon was successfully loaded from the file.")
     except:
         draw_status(f"Err. Load {image_path}")
+
 
 def save_icon():
     draw_status(f"save")
@@ -163,7 +202,7 @@ def save_icon():
     # Set the values in the icon array based on the data in the editor icon
     for y in range(icon_h):
         for x in range(icon_w):
-            icon_array[x][y] = 0 if icon_data[x][15-y] else 255  # Horizontal flip
+            icon_array[x][y] = 0 if icon_data[x][y] else 255
 
     # Create a surface for the icon and rotate it 180 degrees (upside down)
     icon_surface = pygame.surfarray.make_surface(icon_array)
@@ -174,32 +213,42 @@ def save_icon():
     print(f"The icon was successfully saved to the file {image_path}.")
 
 
-def clear_icon():
+def icon_clear():
     draw_status(f"clear > 0")
     icon_data.fill(False)
 
 
-def fill_icon():
+def icon_fill():
     draw_status(f"fill > 1")
     for y in range(icon_w):
         for x in range(icon_h):
             icon_data[x][y] = True
 
 
-def invert_icon():
+def icon_invert():
     draw_status(f"invert 0<->1")
     for y in range(icon_w):
         for x in range(icon_h):
             icon_data[x][y] ^= True
 
 
-def noise_icon():
+def icon_noise():
     draw_status(f"noise")
     for y in range(icon_w):
         for x in range(icon_h):
             icon_data[x][y] = random.randint(0, 1) # True
 
 
+def icon_border():
+    draw_status(f"border")
+    for y in range(icon_h): # L vert.
+        icon_data[0][y] = 1
+        icon_data[icon_w-1][y] = 1
+
+    for x in range(icon_w):
+        icon_data[x][0] = 1
+        icon_data[x][icon_h-1] = 1
+    
 def matrix_icon():
     try:
         for y, row in enumerate(my_icon_matrix):
@@ -238,7 +287,7 @@ def data_icon():
 
 # =================== main loop ===============================
 running = True
-draw_status("Start >")
+draw_status(f"Start > {image_path}")
 
 while running:
     for event in pygame.event.get():
@@ -255,7 +304,7 @@ while running:
                 draw_status(f"x:{pixel_x} | y:{pixel_y} > 1")
 
                 try:
-                    icon_data[pixel_x][15-pixel_y] = True
+                    icon_data[pixel_x][pixel_y] = 1
                 except:
                     print("out of range")    
                 draw_edit_icon()
@@ -270,7 +319,7 @@ while running:
                 draw_status(f"x:{pixel_x} | y:{pixel_y} > 0")
 
                 try:
-                    icon_data[pixel_x][15-pixel_y] = False
+                    icon_data[pixel_x][pixel_y] = False
                 except:
                     print("out of range")
                 draw_edit_icon()
@@ -281,6 +330,9 @@ while running:
 
             elif event.key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
                 save_icon()
+            
+            elif event.key == pygame.K_r and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                mode,icon_w,icon_h = icon_mode(mode,icon_w,icon_h)
 
             elif event.key == pygame.K_z and pygame.key.get_mods() & pygame.KMOD_CTRL:
                 resize += 1
@@ -294,21 +346,23 @@ while running:
                     resize = 1
                 draw_status2(f"zoom {resize}")
 
-            #elif event.key == pygame.K_c:
             elif event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                clear_icon()
+                icon_clear()
 
             elif event.key == pygame.K_d and pygame.key.get_mods() & pygame.KMOD_CTRL: # create save data matrix
                 data_icon()
 
             elif event.key == pygame.K_f and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                fill_icon()
+                icon_fill()
 
             elif event.key == pygame.K_n and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                noise_icon()
+                icon_noise()
+
+            elif event.key == pygame.K_b and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                icon_border()
 
             elif event.key == pygame.K_i and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                invert_icon()
+                icon_invert()
 
             elif event.key == pygame.K_m and pygame.key.get_mods() & pygame.KMOD_CTRL: # import/load data matrix
                 matrix_icon()
@@ -320,7 +374,7 @@ while running:
                 # after Enter
                 draw_status(f"input text: {input_text}")
                 if len(input_text)>2:
-                    new_file = f"{path}{input_text}{icon_w}x{icon_h}.bmp"
+                    new_file = f"{path}{input_text}_{icon_w}.bmp"
                     draw_status2(new_file)
                     image_path = new_file
                 input_text = ""
